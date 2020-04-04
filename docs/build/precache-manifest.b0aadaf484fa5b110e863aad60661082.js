0e7ca6941701df89bdc2d9bab6d4d3962b6b0fb1
Elvira <elvi_ac94@hotmail.com>
2020-04-04 21:53:58 +0200

    create build

---
+++ docs/build/precache-manifest.b0aadaf484fa5b110e863aad60661082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6047ef5dbeb8c441bdefbc9663f2fc0f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc0f016ffc1b311b1ae9",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82b03c9ad15df23c8502",
+    "url": "./static/js/2.2253381e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2253381e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0f016ffc1b311b1ae9",
+    "url": "./static/js/main.869b7a77.chunk.js"
+  },
+  {
+    "revision": "97ed66f44e9a828b1e57",
+    "url": "./static/js/runtime-main.a845a9c5.js"
+  }
+]);
